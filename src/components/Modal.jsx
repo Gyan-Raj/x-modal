fc98544dfbb00046e32e954708cb4d34c738879d
dev2 <dev2@techpyramid.com>
2024-07-14 14:52:45 +0530

    fixed closing of form when clicked outside by adding a parent div to modal class

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -45,67 +45,69 @@ const Modal = () => {
   };
 
   return (
-    <div className="modal">
-      <h1>User Details Modal</h1>
-      <button onClick={() => setIsModalOpen(true)}>Open Form</button>
-      {isModalOpen && (
-        <div className="modal-content" onClick={handleModal}>
-          <form action="" onSubmit={handleSubmit} onClick={handleFormModal}>
-            <h1>Fill Details</h1>
-            <div>
-              <label htmlFor="username">Username:</label>
-              <br />
-              <input
-                id="username"
-                type="text"
-                value={formData.userName}
-                name="userName"
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="email">Email Address:</label>
-              <br />
-              <input
-                id="email"
-                type="email"
-                value={formData.userEmail}
-                name="userEmail"
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="phone">Phone Number:</label>
-              <br />
-              <input
-                id="phone"
-                type="tel"
-                value={formData.userContactNo}
-                name="userContactNo"
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="dob">Date of Birth:</label>
-              <br />
-              <input
-                id="dob"
-                type="date"
-                value={formData.userDob}
-                name="userDob"
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <button type="submit" className="submit-button">
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-      )}
+    <div>
+      <div className="modal">
+        <h1>User Details Modal</h1>
+        <button onClick={() => setIsModalOpen(true)}>Open Form</button>
+        {isModalOpen && (
+          <div className="modal-content" onClick={handleModal}>
+            <form action="" onSubmit={handleSubmit} onClick={handleFormModal}>
+              <h1>Fill Details</h1>
+              <div>
+                <label htmlFor="username">Username:</label>
+                <br />
+                <input
+                  id="username"
+                  type="text"
+                  value={formData.userName}
+                  name="userName"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email Address:</label>
+                <br />
+                <input
+                  id="email"
+                  type="email"
+                  value={formData.userEmail}
+                  name="userEmail"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="phone">Phone Number:</label>
+                <br />
+                <input
+                  id="phone"
+                  type="tel"
+                  value={formData.userContactNo}
+                  name="userContactNo"
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="dob">Date of Birth:</label>
+                <br />
+                <input
+                  id="dob"
+                  type="date"
+                  value={formData.userDob}
+                  name="userDob"
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <button type="submit" className="submit-button">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
